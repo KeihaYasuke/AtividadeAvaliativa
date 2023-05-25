5685260c9cc633f63ca27dc3fa51132497c8d633
AETHER <123389383+jvzinnnn@users.noreply.github.com>
2023-05-25 17:12:50 -0300

    isso ai

--- Atividade Avaliativa I/Assets/codigos/movimento.cs
+++ Atividade Avaliativa I/Assets/codigos/movimento.cs
@@ -4,34 +4,85 @@ using UnityEngine;
 
 public class movimento : MonoBehaviour
 {
-    private Rigidbody2D jogador;
-    public int velocidade;
-    public float forcaPulo;
-    public bool sensor;
+    public int velocidade;                                                          // define a velocidade de movimentação
+    private Rigidbody2D player;                                             // criar variável para percebeer os componentes de física
+    public float forcaPulo;                                              //define a força do pulo
+    public Vector3 rplayer;
 
-    public Transform posicaoSensor;                                  
-    public LayerMask layerChao;
 
-    // Start is called before the first frame update
+    public bool sensor;                                                 // sensor para verificar se está colidindo com o chão
+    public Transform posicaoSensor;                                     //posição onde o sensor será posicionado
+    public LayerMask layerChao;                                         // camada de interação
+
+    private Animator anim;
+    private SpriteRenderer sprite;
+    
+    public GameObject projetil;
+    public Transform PosicaoProjetil;
+
+    public float velocite;
+
+
+
     void Start()
     {
-        jogador = GetComponent<Rigidbody2D>();
-
+        player = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        rplayer = transform.localScale;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         float movimentoX = Input.GetAxisRaw("Horizontal");
-        jogador.velocity = new Vector2(movimentoX * velocidade, jogador.velocity.y);
+        player.velocity = new Vector2(movimentoX * velocidade, player.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && sensor == true)
+
+        if (Input.GetButtonDown("Jump") && sensor == true) //movimento de pulo atravez do sensor
         {
-            jogador.AddForce(new Vector2(0, forcaPulo));
+            player.AddForce(new Vector2(0, forcaPulo));
         }
+
+        anim.SetBool("jump", sensor);
+
+
+
+
+
+        anim.SetInteger("run", (int)movimentoX); //animação de correr
+
+        if (movimentoX > 0)
+        {
+
+            rplayer = new Vector3(1,transform.localScale.y,transform.localScale.z);
+            velocite = 10;
+        }
+        else if (movimentoX < 0)
+        {
+            rplayer = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            velocite = -10;
+        
+        }
+
+
+        if (Input.GetButtonDown("Fire1")) //animação de bater
+        {
+            GameObject temp = Instantiate(projetil);
+
+            temp.transform.position = PosicaoProjetil.position;
+
+
+            temp.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(velocite, 0);
+
+        }
+
+
     }
+
     private void FixedUpdate()
     {
+        //cria um sensor em posição com o raio e layer tambem definida
         sensor = Physics2D.OverlapCircle(posicaoSensor.position, 0.1f, layerChao);
 
     }
